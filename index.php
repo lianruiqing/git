7773e81139b942eefcb3c1c3ed4cc42d71e42794
lianruiqing <lianruiqing@qq.com>
2022-04-13 15:51:27 +0800

    第一次提交

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+	$user='root';
+	$pwd = 'root';
+	$pdo = new PDO('mysql:host=127.0.0.1;dbname=test',$user,$pwd);
+	try{
+		$sql = 'select * from user;';
+		$query = $pdo->query($sql);
+		die;
+
+	}catch(Exception $e){
+		
+		var_dump($e->getMessage());
+		die;
+	}
+
+
